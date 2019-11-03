0d21a5d89f24bf2784cfb726ea82fac1f3afde3e
AlexPetul <47738904+AlexPetul@users.noreply.github.com>
2019-11-04 00:26:00 +0300

    code refactoring, unload routine edited

--- Driver/DriverEntry/mydriver.c
+++ Driver/DriverEntry/mydriver.c
@@ -8,63 +8,60 @@
 NTSTATUS DriverEntry(IN PDRIVER_OBJECT DriverObject, IN PUNICODE_STRING RegistryPath);
 VOID UnloadRoutine(IN PDRIVER_OBJECT DriverObject);
 VOID ProcCallback(HANDLE hParentId, HANDLE processId, BOOLEAN bCreate);
-NTSTATUS PsLookupProcessByProcessId(
-  HANDLE    ProcessId,
-  PEPROCESS *Process
-);
+NTSTATUS PsLookupProcessByProcessId(HANDLE ProcessId, PEPROCESS *Process);
 char* PsGetProcessImageFileName(IN PEPROCESS Process);
-
-
-void RunProcess();
 
 #pragma alloc_text(INIT, DriverEntry)
 #pragma alloc_text(PAGE, UnloadRoutine)
-#pragma alloc_text(PAGE, RunProcess)
  
 NTSTATUS DriverEntry(IN PDRIVER_OBJECT DriverObject, IN PUNICODE_STRING RegistryPath)
 {
-  DriverObject->DriverUnload = UnloadRoutine;
-  DbgPrint("Hello world!\n");
-  PsSetCreateProcessNotifyRoutine(ProcCallback, FALSE);
-  return STATUS_SUCCESS;
+	DriverObject->DriverUnload = UnloadRoutine;
+	DbgPrint("Driver loaded!\n");
+	PsSetCreateProcessNotifyRoutine(ProcCallback, FALSE);
+	return STATUS_SUCCESS;
 }
  
 VOID UnloadRoutine(IN PDRIVER_OBJECT DriverObject)
 {
-  DbgPrint("Goodbye!\n");
+	PsSetCreateProcessNotifyRoutine(&ProcCallback, TRUE);
+	DbgPrint("Driver unloaded!\n");
 }
 
 VOID ProcCallback(HANDLE hParentId, HANDLE processId, BOOLEAN bCreate)
 {
+	UNICODE_STRING sourceProcessName;
+	PEPROCESS sourceProcess, pepSourceProcess;
+	HANDLE fileHandle, processToTerminate;
+	CHAR bufferToWrite[BUFFER_SIZE], bufferToRead[BUFFER_SIZE];
+	char* processName;
+	size_t cbLen;
+	OBJECT_ATTRIBUTES fileAttributes, sourceProjectAttributes;
+	IO_STATUS_BLOCK ioStatusBlock;
+	CLIENT_ID clientID;
+	DWORD pidFromFile;
+	NTSTATUS ntstatus;
+	LARGE_INTEGER byteOffset;
 	if (bCreate == TRUE){
-		UNICODE_STRING uniName;
-		PEPROCESS process;
-		HANDLE handle;
-		CHAR buffer[BUFFER_SIZE];
-		char* processName;
-		size_t cb;
-		OBJECT_ATTRIBUTES objAttr;
-		IO_STATUS_BLOCK ioStatusBlock;
-		NTSTATUS ntstatus;
-		PsLookupProcessByProcessId(processId, &process);
-		processName = PsGetProcessImageFileName(process);
-		ObDereferenceObject(process);
+		PsLookupProcessByProcessId(processId, &sourceProcess);
+		processName = PsGetProcessImageFileName(sourceProcess);
+		ObDereferenceObject(sourceProcess);
 		if (strcmp(processName, "test.exe") == 0){
-			RtlInitUnicodeString(&uniName, L"\\DosDevices\\C:\\proc.txt");
-			InitializeObjectAttributes(&objAttr, &uniName, OBJ_CASE_INSENSITIVE | OBJ_KERNEL_HANDLE, NULL, NULL);
-			ntstatus = ZwCreateFile(&handle, GENERIC_WRITE, &objAttr, &ioStatusBlock, NULL, FILE_ATTRIBUTE_NORMAL, 0, FILE_OVERWRITE_IF, FILE_SYNCHRONOUS_IO_NONALERT, NULL, 0);
+			RtlInitUnicodeString(&sourceProcessName, L"\\DosDevices\\C:\\proc.txt");
+			InitializeObjectAttributes(&fileAttributes, &sourceProcessName, OBJ_CASE_INSENSITIVE | OBJ_KERNEL_HANDLE, NULL, NULL);
+			ntstatus = ZwCreateFile(&fileHandle, GENERIC_WRITE, &fileAttributes, &ioStatusBlock, NULL, FILE_ATTRIBUTE_NORMAL, 0, FILE_OVERWRITE_IF, FILE_SYNCHRONOUS_IO_NONALERT, NULL, 0);
 			if (NT_SUCCESS(ntstatus)){
-				ntstatus = RtlStringCbPrintfA(buffer, sizeof(buffer), "create process");
+				ntstatus = RtlStringCbPrintfA(bufferToWrite, sizeof(bufferToWrite), "create process");
 				if (NT_SUCCESS(ntstatus)){
-					ntstatus = RtlStringCbLengthA(buffer, sizeof(buffer), &cb);
+					ntstatus = RtlStringCbLengthA(bufferToWrite, sizeof(bufferToWrite), &cbLen);
 					if (NT_SUCCESS(ntstatus)){
-						ntstatus = ZwWriteFile(handle, NULL, NULL, NULL, &ioStatusBlock, buffer, cb, NULL, NULL);
+						ntstatus = ZwWriteFile(fileHandle, NULL, NULL, NULL, &ioStatusBlock, bufferToWrite, cbLen, NULL, NULL);
 						if (!NT_SUCCESS(ntstatus)){
 							DbgPrint("Error writing to file\n");
 						}
 						DbgPrint("Process %s opened", processName);
 						DbgPrint("Process calc.exe started");
-						ZwClose(handle);
+						ZwClose(fileHandle);
 					}
 					else{
 						DbgPrint("Error RtlStringCbLengthA\n");
@@ -80,41 +77,45 @@ VOID ProcCallback(HANDLE hParentId, HANDLE processId, BOOLEAN bCreate)
 		}
 	}
 	else{
-		OBJECT_ATTRIBUTES objectAttributes;
-		CLIENT_ID clientID;
-		UNICODE_STRING uniName;
-		DWORD pidFromFile;
-		HANDLE handle, processToTerminate;
-		CHAR buffer[BUFFER_SIZE];
-		size_t cb;
-		OBJECT_ATTRIBUTES objAttr;
-		char* processName;
-		IO_STATUS_BLOCK ioStatusBlock;
-		NTSTATUS ntstatus;
-		LARGE_INTEGER byteOffset;
-		PEPROCESS pProcess;
-		PsLookupProcessByProcessId(processId, &pProcess);
-		processName = PsGetProcessImageFileName(pProcess);
-		ObDereferenceObject(pProcess);
+		PsLookupProcessByProcessId(processId, &pepSourceProcess);
+		processName = PsGetProcessImageFileName(pepSourceProcess);
+		ObDereferenceObject(pepSourceProcess);
 		if (strcmp(processName, "test.exe") == 0){
 			DbgPrint("Process %s terminated", processName);
-			RtlInitUnicodeString(&uniName, L"\\DosDevices\\C:\\proc.txt");
-			InitializeObjectAttributes(&objAttr, &uniName, OBJ_CASE_INSENSITIVE | OBJ_KERNEL_HANDLE, NULL, NULL);
-			ntstatus = ZwCreateFile(&handle, GENERIC_READ, &objAttr, &ioStatusBlock, NULL, FILE_ATTRIBUTE_NORMAL, 0, FILE_OPEN, FILE_SYNCHRONOUS_IO_NONALERT, NULL, 0);
+			RtlInitUnicodeString(&sourceProcessName, L"\\DosDevices\\C:\\proc.txt");
+			InitializeObjectAttributes(&fileAttributes, &sourceProcessName, OBJ_CASE_INSENSITIVE | OBJ_KERNEL_HANDLE, NULL, NULL);
+			ntstatus = ZwCreateFile(&fileHandle, GENERIC_READ, &fileAttributes, &ioStatusBlock, NULL, FILE_ATTRIBUTE_NORMAL, 0, FILE_OPEN, FILE_SYNCHRONOUS_IO_NONALERT, NULL, 0);
 			if(NT_SUCCESS(ntstatus)) {
 				byteOffset.LowPart = byteOffset.HighPart = 0;
-				ntstatus = ZwReadFile(handle, NULL, NULL, NULL, &ioStatusBlock, buffer, BUFFER_SIZE, &byteOffset, NULL);
+				ntstatus = ZwReadFile(fileHandle, NULL, NULL, NULL, &ioStatusBlock, bufferToRead, BUFFER_SIZE, &byteOffset, NULL);
 				if(NT_SUCCESS(ntstatus)) {
-					buffer[BUFFER_SIZE-1] = '\0';
-					pidFromFile = atoi(buffer);
-					InitializeObjectAttributes( &objectAttributes, NULL, OBJ_KERNEL_HANDLE, NULL, NULL );
+					bufferToRead[BUFFER_SIZE-1] = '\0';
+					pidFromFile = atoi(bufferToRead);
+					InitializeObjectAttributes( &sourceProjectAttributes, NULL, OBJ_KERNEL_HANDLE, NULL, NULL );
 					clientID.UniqueProcess = (HANDLE)pidFromFile; 
 					clientID.UniqueThread = NULL;
-					ZwOpenProcess(&processToTerminate, DELETE, &objectAttributes, &clientID);
-					ZwTerminateProcess(processToTerminate, 0);
-					ZwClose(processToTerminate);
+					ntstatus = ZwOpenProcess(&processToTerminate, DELETE, &sourceProjectAttributes, &clientID);
+					if (NT_SUCCESS(ntstatus)){
+						ntstatus = ZwTerminateProcess(processToTerminate, 0);
+						if (NT_SUCCESS(ntstatus)){
+							ZwClose(processToTerminate);
+							DbgPrint("Process calc.exe terminated");
+						}
+						else{
+							DbgPrint("Error terminating target process");
+						}
+					}
+					else{
+						DbgPrint("Error opening target process");
+					}
 				}
-				ZwClose(handle);
+				else{
+					DbgPrint("Error reading file");
+				}
+				ZwClose(fileHandle);
+			}
+			else{
+				DbgPrint("Error opening file");
 			}
 		}
 	}
